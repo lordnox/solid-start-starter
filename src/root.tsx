c4b5343c69e2edf96668013ab596d639e40542d1
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-19 10:03:30 +0100

    added env, added zod, added prettier

--- src/root.tsx
+++ src/root.tsx
@@ -1,26 +1,12 @@
 // @refresh reload
-import { Suspense } from "solid-js";
-import {
-  useLocation,
-  A,
-  Body,
-  ErrorBoundary,
-  FileRoutes,
-  Head,
-  Html,
-  Meta,
-  Routes,
-  Scripts,
-  Title,
-} from "solid-start";
-import "./root.css";
+import { Suspense } from 'solid-js'
+import { useLocation, A, Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title } from 'solid-start'
+import './root.css'
 
 export default function Root() {
-  const location = useLocation();
+  const location = useLocation()
   const active = (path: string) =>
-    path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+    path == location.pathname ? 'border-sky-600' : 'border-transparent hover:border-sky-600'
   return (
     <Html lang="en">
       <Head>
@@ -33,10 +19,10 @@ export default function Root() {
           <ErrorBoundary>
             <nav class="bg-sky-800">
               <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+                <li class={`border-b-2 ${active('/')} mx-1.5 sm:mx-6`}>
                   <A href="/">Home</A>
                 </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
+                <li class={`border-b-2 ${active('/about')} mx-1.5 sm:mx-6`}>
                   <A href="/about">About</A>
                 </li>
               </ul>
@@ -49,5 +35,5 @@ export default function Root() {
         <Scripts />
       </Body>
     </Html>
-  );
+  )
 }
