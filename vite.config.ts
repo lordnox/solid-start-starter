bee6da30b38bb91119a326a62f4d24a832d9735e
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-20 11:41:18 +0100

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
 
-export default defineConfig({
-  plugins: [solid()],
+export default defineConfig(() => {
+  dotenv.config()
+  return {
+    plugins: [solid()],
+  }
 })
