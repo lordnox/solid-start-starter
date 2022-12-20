0b0facf726be6c6c8fef3dca654bca0e964d1e89
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-20 02:19:10 +0100

    test vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
+import vercel from 'solid-start-vercel'
+import solid from 'solid-start/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [solid()],
-});
+  plugins: [solid({ adapter: vercel() })],
+})
