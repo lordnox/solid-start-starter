95baee83cb5fd7e83538aef3fab9d3eb9bc3adf4
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-20 20:42:56 +0100

    update to use VERCEL_URL

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -9,7 +9,13 @@ export const serverScheme = z.object({
     .transform((value) => value === 'true'),
   AUTH_SECRET: z.string(),
   DATABASE_URL: z.string(),
-  START_BASE_URL: z.string(),
+  START_BASE_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set START_BASE_URL
+    // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    (str) => process.env.VERCEL_URL ?? str,
+    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    process.env.VERCEL ? z.string() : z.string().url(),
+  ),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
 })
