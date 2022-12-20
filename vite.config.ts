42cb9b051aac0f64c539f12eb5147b0596a6bd03
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-20 12:41:45 +0100

    updated stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import solid from 'solid-start/vite'
+// import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 import dotenv from 'dotenv'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig(() => {
   dotenv.config()
   return {
-    plugins: [solid()],
+    plugins: [solidPlugin()],
+    build: {
+      target: 'esnext',
+      polyfillDynamicImport: false,
+    },
   }
 })
