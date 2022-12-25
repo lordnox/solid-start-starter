3e476b555fb4fcc75e074322855cc529d852ddb9
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-25 22:44:35 +0100

    updated client side session handling

---
+++ src/utils/auth.ts
@@ -0,0 +1,31 @@
+import { Session } from '@auth/core'
+import { createRoot, createSignal } from 'solid-js'
+
+type INITIAL_STATE = { state: 'INITIAL' }
+type LOADING_STATE = { state: 'LOADING' }
+export type LOADED_STATE = { state: 'LOADED'; data: Session | null }
+
+type STATE = INITIAL_STATE | LOADING_STATE | LOADED_STATE
+
+const [currentSession, setCurrentSession] = createRoot(() =>
+  createSignal<STATE>({
+    state: 'INITIAL',
+  }),
+)
+
+export const useSession = () => {
+  const session = currentSession()
+  if (session.state === 'INITIAL') {
+    setCurrentSession({ state: 'LOADING' })
+    fetch('/api/session').then(async (response) =>
+      setCurrentSession({
+        state: 'LOADED',
+        data: await response.json(),
+      }),
+    )
+  }
+
+  return currentSession
+}
+
+export const session = () => (useSession()() as LOADED_STATE)?.data
